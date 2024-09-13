ba3a30cdadc5ae1bb8f4b961ea45b1ac97ea8a4b
Max <maxwangko1@gmail.com>
2024-09-13 17:56:37 -0400

    project init

---
+++ Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by Max Ko on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
