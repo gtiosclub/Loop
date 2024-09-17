09aa67d707df35f88d9214f5350e81c47ae2a486
jnair9 <143296724+jnair9@users.noreply.github.com>
2024-09-17 15:47:47 -0400

    Uploaded Skeleton Code for base homepage

---
+++ Loop/Model/Post.swift
@@ -0,0 +1,20 @@
+//
+//  Post.swift
+//  Loop
+//
+//  Created by Jason Nair on 9/17/24.
+//
+
+import Foundation
+    //create a struct that will hold some basic information contained within a post
+    /*
+     POST:
+        - id = UUID()
+        - username: string
+        - content: string
+        - date
+     */
+struct Post: Identifiable {
+    let id = UUID()
+    //FILL IN THE REST
+}
---
+++ Loop/ViewModel/FeedViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  FeedViewModel.swift
+//  Loop
+//
+//  Created by Jason Nair on 9/17/24.
+//
+
+import Foundation
+
+/*
+ This is the viewmodel that will connect the model of posts to the view where they will be displayed
+ */
+class FeedViewModel: ObservableObject{
+    @Published var posts: [Post] = []
+    
+    //the init should call a function called loadPosts that will contain an array of mock data Posts
+    init(){
+        //TODO
+    }
+    //fill in this function, an array posts of Post()
+    func loadPosts() {
+        //TODO
+    }
+}
