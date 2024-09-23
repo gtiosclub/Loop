72b9dc17287907f731b985d63639b6a078ec3688
joey <joey@lawn-143-215-55-211.lawn.gatech.edu>
2024-09-23 18:07:23 -0400

    cleanup

--- Loop/Views/Challenges/Models/CardView.swift
+++ Loop/Views/Challenges/Models/CardView.swift
@@ -5,15 +5,14 @@
 //  Created by joey on 9/23/24.
 //
 
-// CardView.swift
 import SwiftUI
 
 struct CardView: View {
-    let challenge: Challenge  // Use Challenge instead of DailyScrum
+    let challenge: Challenge
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(challenge.title)  // Update to challenge.title
+            Text(challenge.title)
                 .font(.headline)
             Spacer()
             HStack {
@@ -25,14 +24,14 @@ struct CardView: View {
             .font(.caption)
         }
         .padding()
-        .foregroundColor(challenge.theme.accentColor)  // Use challenge.theme.accentColor
+        .foregroundColor(challenge.theme.accentColor)
     }
 }
 
 struct CardView_Previews: PreviewProvider {
-    static var challenge = Challenge.sampleData[0]  // Use Challenge sample data
+    static var challenge = Challenge.sampleData[0]
     static var previews: some View {
-        CardView(challenge: challenge)  // Pass challenge to the preview
+        CardView(challenge: challenge)
             .background(challenge.theme.mainColor)
             .previewLayout(.fixed(width: 400, height: 60))
     }
