0cfa9b1bad98c3fe47676b9ccf39d6baa37cdd26
Kevin Zhang <kzhang0826@gmail.com>
2024-09-24 19:49:39 -0400

    Update ManageFriendsView.swift

--- Loop/Views/Profile/ManageFriendsView.swift
+++ Loop/Views/Profile/ManageFriendsView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ManageFriendsView: View {
+    @State private var searchText: String = ""
+    @State private var friends: [String] = ["friend 1", "friend 2", "friend 3"]
+    @State private var allFriends: [String] = ["friend 1", "friend 2", "friend 3", "friend 4", "friend 5", "friend 6"]
+    @State private var filteredFriends: [String] = []
+    
     var body: some View {
         Text("Manage Friends View")
     }
