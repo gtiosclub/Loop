3c899942b5670f76f448be9bcd7442b25817d1cf
Matt Free <mjfree34@gmail.com>
2024-09-26 02:33:04 -0400

    Added watch target

---
+++ LoopWatchApp Watch App/LoopWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopWatchAppApp.swift
+//  LoopWatchApp Watch App
+//
+//  Created by Matt Free on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LoopWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
