c419c76976688cffc98e8f6d5b8737f1bd90cde3
adilhusain05 <adil-gt@outlook.com>
2024-09-26 16:33:22 -0400

    User Model

--- Loop/Models/User.swift
+++ Loop/Models/User.swift
@@ -2,7 +2,37 @@
 //  User.swift
 //  Loop
 //
-//  Created by Ryan O’Meara on 9/19/24.
+//  Created by Adil Husain on 9/26/24.
 //
 
 import Foundation
+
+struct User {
+    var uid: String
+    var name: String
+    var challengeIds: [String]
+    var profilePictureId: String
+    var friends: [String]
+    var incomingRequest: [String]
+
+    init(uid: String, name: String, challengeIds: [String], profilePictureId: String, friends: [String], incomingRequest: [String]) {
+        self.uid = uid
+        self.name = name
+        self.challengeIds = challengeIds
+        self.profilePictureId = profilePictureId
+        self.friends = friends
+        self.incomingRequest = incomingRequest
+    }
+
+    // Convert User to a dictionary for Firestore storage
+    func toDictionary() -> [String: Any] {
+        return [
+            "uid": uid,
+            "name": name,
+            "challengeIds": challengeIds,
+            "profilePictureId": profilePictureId,
+            "friends": friends,
+            "incomingRequest": incomingRequest
+        ]
+    }
+}
