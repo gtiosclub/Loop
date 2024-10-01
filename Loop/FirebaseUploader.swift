8080de22d7eee590611b506fb874cf4893c965f2
Shrey Agarwal <agshrey@gmail.com>
2024-10-01 19:11:56 -0400

    image upload feature

--- Loop/FirebaseUploader.swift
+++ Loop/FirebaseUploader.swift
@@ -12,23 +12,18 @@ import UIKit
 class FirebaseUploader {
     
     static func uploadPhoto(image: UIImage?) {
-        // Create storage reference
         let storageRef = Storage.storage().reference()
         
-        //Check if image can turn into data
         let imageData = image!.jpegData(compressionQuality: 0.8)
         
         guard imageData != nil else {
             return
         }
         
-        //Specify file path and name
         let fileRef = storageRef.child("images/\(UUID().uuidString).jpg")
         
-        // Upload that data
         let uploadTask = fileRef.putData(imageData!, metadata: nil) { metadata, error in
             
-            //Check for errors
             if error == nil && metadata != nil {
                 print("There was an error when uploading.")
             }
