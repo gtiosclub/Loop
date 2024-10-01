8045edea99ad9a940b7c4079be7a6be72a75ca10
VGandrade <148589849+VGandrade@users.noreply.github.com>
2024-10-01 19:22:27 -0400

    created feed view

---
+++ Loop/Views/HomePage/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  Loop
+//
+//  Created by Jason Nair on 9/17/24.
+//
+
+import SwiftUI
+//this is the view for individual posts please use the correct stack and format the post so that it has the username, content, and timestamp of each post, feel free to add decoration.
+struct PostView: View {
+    var post : Post
+    var body: some View {
+        //TODO
+        Text("FILL IN")
+    }
+}
+
+//#Preview {
+//    PostView(post: Post)
+//}
