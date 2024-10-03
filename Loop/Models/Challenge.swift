ab23a75820738b8ce22642d4e77eda1c6322a59d
josephmasson26 <c.p.joseph18@gmail.com>
2024-10-03 16:00:24 -0400

    clean sweep hopfully

--- Loop/Models/Challenge.swift
+++ Loop/Models/Challenge.swift
@@ -100,31 +100,7 @@ struct Challenge: Identifiable {  // Previously DailyScrum
         }
     }
     
-    enum CodingKeys: String, CodingKey {
-        case id
-        case title
-        case attendees
-        case lengthInMinutes
-        case theme
-        case endDate
-        case challengeType
-        case dataMeasured
-        case dateCreated
-    }
     
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decodeIfPresent(UUID.self, forKey: .id) ?? UUID()
-        title = try container.decode(String.self, forKey: .title)
-        
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(title, forKey: .title)
-        
-    }
     
     
 }
