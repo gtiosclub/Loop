d02a32550ccfbb5f63bc6cb6107fbb26d9bd7191
Ethan Kim <jikim1803@gmail.com>
2024-10-03 20:02:59 -0400

    Added addFriendView

--- Loop/Views/Profile/AddFriendsView.swift
+++ Loop/Views/Profile/AddFriendsView.swift
@@ -1,13 +1,13 @@
 //
-//  ManageFriendsView.swift
+//  AddFriendsView.swift
 //  Loop
 //
-//  Created by Ryan O’Meara on 9/19/24.
+//  Created by Jihoon Kim on 10/3/24.
 //
 
 import SwiftUI
 
-struct FriendRow: View {
+struct AddFriendRow: View {
     var friendName: String
     var status: String
     
@@ -19,28 +19,35 @@ struct FriendRow: View {
                 Text(status).font(.caption).foregroundColor(.gray)
             }
             Spacer()
+            VStack {
+                Button(action: {
+                    print("friend added")
+                }) {Image(systemName: "plus")
+                        .font(.system(size: 24))
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
+            }
         }.padding().background(Color.white).cornerRadius(10).shadow(radius: 2).padding(.horizontal)
     }
 }
 
-struct ManageFriendsView: View {
+struct AddFriendsView: View {
     @State private var searchText: String = ""
-    @State private var onlineFriends: [String] = ["Kevin", "Ethan", "Jason"]
-    @State private var offlineFriends: [String] = ["Kevin", "Ethan", "Jason", "Seohyun", "Dennis", "Aryun", "Dennis", "Dennis", "Dennis"]
-    @State private var allFriends: [String] = ["Kevin", "Ethan", "Jason", "Seohyun", "Dennis", "Aryun", "Dennis", "Dennis", "Dennis"]
-    @State private var filteredFriends: [String] = []
-    
+    @State private var allFriends: [String] = ["Kevin", "Ethan", "Jason"]
+    @State private var requestingFriends: [String] = ["Me", "Kevin", "Ethan", "Jason"]
+    @State private var matchedFriends: [String] = []
     
     
     private func filterFriends() {
         if searchText.isEmpty {
             // When search is empty, reset to the original friends list
-            onlineFriends = allFriends.filter { $0.contains("1") }  // Example to filter for "Online" friends
-            offlineFriends = allFriends.filter { !$0.contains("1") }  // Example to filter for "Offline" friends
+            matchedFriends = []
         } else {
             // Filter friends by search text
-            onlineFriends = allFriends.filter { $0.lowercased().contains(searchText.lowercased()) && $0.contains("1") }
-            offlineFriends = allFriends.filter { $0.lowercased().contains(searchText.lowercased()) && !$0.contains("1") }
+            matchedFriends = allFriends.filter { $0.lowercased().contains(searchText.lowercased()) && !$0.contains("1") }
         }
     }
     
@@ -55,7 +62,7 @@ struct ManageFriendsView: View {
                 
                 Spacer()
                 
-                Text("Manage Friends").font(.headline)
+                Text("Add Friends").font(.headline)
                 
                 Spacer()
             }.padding()
@@ -66,27 +73,23 @@ struct ManageFriendsView: View {
                 TextField("Search Friends", text: $searchText).padding(8).background(Color.orange.opacity(0.3)).cornerRadius(8).onChange(of: searchText) { filterFriends() }
             }.padding(.horizontal)
             
-
-                VStack(alignment: .leading) {
-                    Text("Online: ").font(.subheadline).padding(.leading)
-                    ScrollView {
-                        ForEach(onlineFriends, id: \.self) {friend in
-                            FriendRow(friendName: friend, status: "Now Active")
-                        }
-                    }
-                }.padding(.top)
-                
             VStack(alignment: .leading) {
-                Text("Offline: ").font(.subheadline).padding(.leading)
                 ScrollView {
-                    ForEach(offlineFriends, id: \.self) {friend in
-                        FriendRow(friendName: friend, status: "Last Seen: 7 days")
+                    ForEach(matchedFriends, id: \.self) {friend in
+                        AddFriendRow(friendName: friend, status: "Now Active")
+                    }
+                }
+            }.padding(.top)
+            VStack(alignment: .leading) {
+                Text("Incoming Requests: ").font(.subheadline).padding(.leading)
+                ScrollView {
+                    ForEach(requestingFriends, id: \.self) {friend in
+                        AddFriendRow(friendName: friend, status: "Last Seen: 7 days")
                     }
                 }
             }
             
-            
-            Spacer()
+                
             
             
         }.background(Color.gray.opacity(0.1).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
@@ -95,5 +98,5 @@ struct ManageFriendsView: View {
 }
 
 #Preview {
-    ManageFriendsView()
+    AddFriendsView()
 }
