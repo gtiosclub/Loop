62e695b4f62d9395ab21546232e7a27184486768
ryan-omeara <ryanomeara12@gmail.com>
2024-10-06 15:53:57 -0400

    added basic components for create challenge

--- Loop/Views/Challenges/CreateChallengeView.swift
+++ Loop/Views/Challenges/CreateChallengeView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct CreateChallengeView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var challengeName: String = ""
+    @State private var challengeType: ChallengeType = .mostMilesRan
+    @State private var endDate: Date = Date()
+    private var futureDate: Calendar.current.date(byAdding: .year, value: 1, to: Date())
+    
+    
     var body: some View {
-        Text("Create Challenge View")
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "chevron.left")
+                .foregroundColor(.black)
+                .bold()
+        }
+        //Challenge Name (text input)
+        TextField("Enter your challenge name.", text: $challengeName)
+        //Challenge Type (dropdown)
+        List {
+            Picker("Challenge Type", selection: $challengeType) {
+                Text("Most Miles Ran").tag(ChallengeType.mostMilesRan)
+                Text("Most Calories Burned").tag(ChallengeType.mostCaloriesBurned)
+                Text("Fastest Single Mile").tag(ChallengeType.fastestMile)
+                Text("Most Consecutive Days Exercised").tag(ChallengeType.mostConsecutiveDays)
+                Text("Farthest Distance Ran").tag(ChallengeType.farthestDistance)
+            }
+        }
+        //End Date (date picker)
+        DatePicker("End Date", selection: $endDate, in: Date.now <...<= futureDate, displayedComponents: .date)
+        //Participants, with add participant (search list)
     }
+}
+
+enum ChallengeType: String, CaseIterable, Identifiable {
+    var id: Self {self}
+    case mostMilesRan
+    case mostCaloriesBurned
+    case fastestMile
+    case mostConsecutiveDays
+    case farthestDistance
 }
 
 #Preview {
