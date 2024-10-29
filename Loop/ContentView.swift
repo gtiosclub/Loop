9b6c81446452f416e962ae0a3d55eb7d9bb5b6a8
Dansterhamster <dbyrd38@gatech.edu>
2024-10-29 17:12:22 -0400

    Fixed merge conflicts

--- Loop/ContentView.swift
+++ Loop/ContentView.swift
@@ -9,23 +9,25 @@ struct ContentView: View {
     @StateObject var healthKitManager = HealthKitManager.shared
     let user: User = User(uid: "user", name: "user", username: "user", profilePictureId: "None")
     var body: some View {
-        Button("Get user data") {
-            Task {
-                await user.addUser()
-                let userGot: User? = await user.getUser(uid: user.uid)
-                if let userGotUnwrapped = userGot {
-                    print("uid: \(userGotUnwrapped.uid)")
-                    print("incomingRequest: \(userGotUnwrapped.incomingRequest)")
-                    print("challengeIds: \(userGotUnwrapped.challengeIds)")
-                    print("name: \(userGotUnwrapped.name)")
-                    print("profilePictureId: \(userGotUnwrapped.profilePictureId)")
-                    print("username: \(userGotUnwrapped.username)")
-                }
-            }
-        }
-        Button("Get steps") {
-            healthKitManager.fetchAllDatas()
-        }
+//        Button("Get user data") {
+//            Task {
+//                await user.addUser()
+//                let userGot: User? = await user.getUser(uid: user.uid)
+//                if let userGotUnwrapped = userGot {
+//                    print("uid: \(userGotUnwrapped.uid)")
+//                    print("incomingRequest: \(userGotUnwrapped.incomingRequest)")
+//                    print("challengeIds: \(userGotUnwrapped.challengeIds)")
+//                    print("name: \(userGotUnwrapped.name)")
+//                    print("profilePictureId: \(userGotUnwrapped.profilePictureId)")
+//                    print("username: \(userGotUnwrapped.username)")
+//                }
+//            }
+//        }
+//        Button("Get steps") {
+//            healthKitManager.fetchAllDatas()
+//        }
+        Text("Hello, World!")
+        
     }
 }
 
