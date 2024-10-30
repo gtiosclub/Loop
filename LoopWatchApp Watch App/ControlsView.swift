8103b428f76993c5e10d65e24f036e7dbbf06b0c
NityaPotti <nityapotti01@gmail.com>
2024-10-30 18:14:49 -0400

    hi

---
+++ LoopWatchApp Watch App/ControlsView.swift
@@ -0,0 +1,37 @@
+//
+//  ControlsView.swift
+//  LoopWatchApp Watch App
+//
+//  Created by Nitya Potti on 10/28/24.
+//
+
+import SwiftUI
+
+struct ControlsView: View {
+    @EnvironmentObject var workoutManager: WorkoutManager
+    var body: some View {
+        HStack {
+            VStack {
+                Button {
+                    workoutManager.endWorkout()
+                } label: {
+                    Image(systemName: "xmark")
+                }
+                tint(Color.red)
+                .font(.title2)
+                Text ("End")
+            }
+            VStack {
+                Button {
+                    workoutManager.togglePause()
+                } label: {
+                    Image(systemName: workoutManager.isRunning ? "Pause" : "Resume")
+                }
+                tint(Color.yellow)
+                .font(.title2)
+                Text (workoutManager.isRunning ? "Pause" : "Resume")
+            }
+            
+        }
+    }
+}
