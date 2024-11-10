299a0a746eafb37eab0eccda85522bd69a323d2d
josephmasson26 <jmasson6@gatech.edu>
2024-11-10 12:09:24 -0500

    Starting data pull - error at HeartRate

--- Loop/ViewModels/RecordViewModel.swift
+++ Loop/ViewModels/RecordViewModel.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 import WatchConnectivity
+import HealthKit
 
 class RecordViewModel: NSObject, ObservableObject, WCSessionDelegate {
     // boolean from the watch to indicate if a workout is in progress
     @Published var workoutInProgress = false
+    let healthStore = HKHealthStore()
 
     override init() {
         super.init()
@@ -53,9 +55,66 @@ class RecordViewModel: NSObject, ObservableObject, WCSessionDelegate {
                 print("Recieved Firestore message from watch: \(updateFirestore)")
                 if updateFirestore {
                     print("Uploading workout data to Firestore")
-                    //self.uploadWorkoutData()
+                    self.uploadWorkoutData()
                 }
             }
         }
+    }
+
+    func uploadWorkoutData() {
+        //pull workout data from HealthKit
+        let workoutType = HKObjectType.workoutType()
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+        let query = HKSampleQuery(sampleType: workoutType, predicate: nil, limit: 1, sortDescriptors: [sortDescriptor]) { _, samples, error in 
+            guard let samples = samples, let workout = samples.first as? HKWorkout else {
+                print("No workout data found or error: \(String(describing: error?.localizedDescription))")
+                return
+            }
+
+            //set up the hk variables
+            let heartRateType = HKObjectType.quantityType(forIdentifier: .heartRate)!
+            let caloriesType = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!
+            let distanceType = HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!
+            print ("Fetched Data")
+
+            let heartRateQuery = HKSampleQuery(sampleType: heartRateType, predicate: HKQuery.predicateForObjects(from: workout), limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, samples, error) in
+                guard let heartRateSamples = samples as? [HKQuantitySample] else {
+                    print("No heart rate data found or error: \(String(describing: error?.localizedDescription))")
+                    return
+                }
+
+                
+                let heartRates = heartRateSamples.map { $0.quantity.doubleValue(for: HKUnit(from: "count/min")) }
+                let averageHeartRate = heartRates.reduce(0, +) / Double(heartRates.count)
+
+                print("Average heart rate: \(averageHeartRate)")
+                
+                let caloriesQuery = HKSampleQuery(sampleType: caloriesType, predicate: HKQuery.predicateForObjects(from: workout), limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, samples, error) in
+                    guard let caloriesSamples = samples as? [HKQuantitySample] else {
+                        print("No calories data found or error: \(String(describing: error?.localizedDescription))")
+                        return
+                    }
+                    
+                    let totalCalories = caloriesSamples.map { $0.quantity.doubleValue(for: .kilocalorie()) }.reduce(0, +)
+
+                    print("Total calories: \(totalCalories)")
+
+                    let distanceQuery = HKSampleQuery(sampleType: distanceType, predicate: HKQuery.predicateForObjects(from: workout), limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, samples, error) in
+                        guard let distanceSamples = samples as? [HKQuantitySample] else {
+                            print("No distance data found or error: \(String(describing: error?.localizedDescription))")
+                            return
+                        }
+                        
+                        let totalDistance = distanceSamples.map { $0.quantity.doubleValue(for: .mile()) }.reduce(0, +)
+                        print("Total distance: \(totalDistance)")
+                    }
+                    self.healthStore.execute(distanceQuery)
+                }
+                self.healthStore.execute(caloriesQuery)
+            }
+            self.healthStore.execute(heartRateQuery)
+        }
+        healthStore.execute(query)
+        
     }
 }
