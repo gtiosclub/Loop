ede3f7dc483d04e357450bc99939a01f8671db4d
Jason Wrzesien <jason.wrzesien@icloud.com>
2024-11-11 15:21:30 -0500

    Updated Card View

--- Loop/Views/Challenges/CardView.swift
+++ Loop/Views/Challenges/CardView.swift
@@ -11,19 +11,82 @@ struct CardView: View {
     let challenge: Challenge
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(challenge.title)
-                .font(.headline)
-            Spacer()
-            HStack {
-                Label("\(challenge.attendees.count)", systemImage: "person.3")
-                Spacer()
-                Label("\(challenge.dateCreated.formatted(.dateTime.day().month(.twoDigits))) - \(challenge.endDate.formatted(.dateTime.day().month(.twoDigits)))", systemImage: "clock")
+        NavigationStack {
+            VStack(spacing: 0) {
+                ZStack {
+                    Color(.lightGray)
+                    
+                    VStack {
+                        HStack {
+                            Text(challenge.title)
+                                .font(.system(size: 26))
+                                .padding([.leading, .top], 15).padding(.bottom, 0.5)
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Label("04d 02h 44m", systemImage: "clock")
+                                .padding(.leading, 15)
+                            Spacer()
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .foregroundStyle(.white)
+                .frame(height: 230)
+                .clipShape(.rect(topLeadingRadius: 15, topTrailingRadius: 15))
+                
+                
+                ZStack {
+                    Color(.lightGray).brightness(0.15)
+                    
+                    VStack {
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .fill(Color(.lightGray))
+                                    .frame(width: 30, height: 30)
+                                Circle()
+                                    .stroke(Color(.lightGray), lineWidth: 0)
+                                    .fill(Color(.lightGray))
+                                    .frame(width: 30, height: 30)
+                                    .padding(.leading, 45)
+                                Circle()
+                                    .stroke(Color(.lightGray), lineWidth: 0)
+                                    .fill(Color(.lightGray))
+                                    .frame(width: 30, height: 30)
+                                    .padding(.leading, 90)
+                            }
+                            .padding(.leading, -30)
+                            
+                            Text(String(challenge.attendees.count) + " Participants")
+                            
+                            Spacer()
+                            
+                            Label("Oct 10 - Nov 1", systemImage: "calendar")
+                                .padding(.trailing, 20)
+                        }
+                        
+                        NavigationLink {
+                            ChallengeView(challenge: challenge)
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color(.lightGray))
+                                    .frame(width: 350, height: 40)
+                                
+                                Text("View Challenge")
+                                    .font(.system(size: 16, weight: .medium))
+                            }
+                        }
+                        .foregroundStyle(.black)
+                    }
+                }
+                .frame(height: 100)
+                .clipShape(.rect(bottomLeadingRadius: 15, bottomTrailingRadius: 15))
             }
-            .font(.caption)
         }
-        .padding()
-        .foregroundColor(challenge.theme.accentColor)
     }
 }
 
@@ -31,7 +94,6 @@ struct CardView_Previews: PreviewProvider {
     static var challenge = Challenge.sampleData[0]
     static var previews: some View {
         CardView(challenge: challenge)
-            .background(challenge.theme.mainColor)
             .previewLayout(.fixed(width: 400, height: 60))
     }
 }
