c8c264e051852c887c3aca8d3d9d33c949763fb6
ryan-omeara <ryanomeara12@gmail.com>
2024-11-19 12:10:19 -0500

    fixed merge conflict

--- Loop/Views/Challenges/ChalLeaderboardView.swift
+++ Loop/Views/Challenges/ChalLeaderboardView.swift
@@ -59,11 +59,13 @@ struct ChalLeaderboardView: View {
                             Circle()
                                 .foregroundColor(.gray)
                                 .frame(width: 65, height: 65)
+                                .padding(.leading, 15)
                         } else {
                             if let url = URL(string: sortedPersonList[index].profilePicURL) {
                                 AsyncImage(url: url) { image in
                                     image.resizable().scaledToFill()
                                         .frame(width: 65, height: 65).clipShape(.circle)
+                                        .padding(.leading, 15)
                                 } placeholder: {
                                     Circle()
                                         .frame(width: 30, height: 30)
@@ -86,41 +88,43 @@ struct ChalLeaderboardView: View {
                         Text(String(format: "%.1f miles", sortedPersonList[index].score))
                             .fontWeight(.medium)
                         
+                            Text(String(index + 1))
+                            Image(systemName: "arrowtriangle.up.fill")
+                            .padding(.trailing, 20)
+                        
                             
-                            HStack {
-                                Circle()
-                                    .foregroundColor(.gray)
-                                    .frame(width: 30, height: 30)
-                                
-                                Text(sortedPersonList[index].name)
-                                    .fontWeight(.light)
-                                
-                                Spacer()
-                                
-                                Text(String(format: "%.1f miles", sortedPersonList[index].score))
-                                    .fontWeight(.medium)
-                                
-                                HStack {
-                                    Text(String(index + 1))
-                                    Image(systemName: "arrowtriangle.up.fill")
-                                        .font(.system(size: 9, weight: .regular))
-                                        .opacity(0.5)
-                                }
-                                .padding(.leading, 30)
-                                .fontWeight(.medium)
-                                
-                            }
-                            .font(.system(size: 14))
-                            .padding(.horizontal, 30).padding(.vertical, 5)
-                            
-                            Divider().frame(width: 375).overlay(.gray)
+//                            HStack {
+//                                Circle()
+//                                    .foregroundColor(.gray)
+//                                    .frame(width: 30, height: 30)
+//                                
+//                                Text(sortedPersonList[index].name)
+//                                    .fontWeight(.light)
+//                                
+//                                Spacer()
+//                                
+//                                Text(String(format: "%.1f miles", sortedPersonList[index].score))
+//                                    .fontWeight(.medium)
+//                                
+//                                HStack {
+//                                    Text(String(index + 1))
+//                                    Image(systemName: "arrowtriangle.up.fill")
+//                                        .font(.system(size: 9, weight: .regular))
+//                                        .opacity(0.5)
+//                                }
+//                                .padding(.leading, 30)
+//                                .fontWeight(.medium)
+//                                
+//                            }
+//                            .font(.system(size: 14))
+//                            .padding(.horizontal, 30).padding(.vertical, 5)
                         }
+                    Divider()
                     }
                 }
             }
         }
     }
-}
 
 #Preview {
     ChalLeaderboardView(personList: [Person(name:"Ryan", score: 4.2), Person(name:"Max", score: 14.8), Person(name:"Jason", score: 7.1), Person(name:"Sam", score: 11.4), Person(name: "Joe", score: 5.5)])
